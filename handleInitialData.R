145f83bfebe4fd1027a89019aeece01ca44027c0
Ricardo Morillo S <ricardo@dreamcode.io>
2020-02-28 21:48:18 -0600

    Add final graph logic

--- handleInitialData.R
+++ handleInitialData.R
@@ -14,6 +14,7 @@
 library(readxl)
 library(tidyverse)
 library(readOffice)
+library(ggplot2)
 
 # Definir nombre del archivo
 baseDeDatos <- "quejas_clientes_electrico_v1.xlsx"
@@ -41,13 +42,47 @@ totalDeDatos <- totalDeDatosComoLista$n
 cat("El total de datos es => ", totalDeDatos)
 
 # Agrupar y contar incidencias por millas
-incidenciasPorMillas <- datosYColumnasMejorados %>% group_by(mileage) %>% count()
+incidenciasPorMillas <- datosYColumnasMejorados %>% filter(mileage < 1000) %>% group_by(mileage) %>% count()
 # Agrupar y contar incidencias por parte
-incidenciasPorParte <- datosYColumnasMejorados %>% group_by(part_item) %>% count()
+incidenciasPorParte <- datosYColumnasMejorados %>% filter(str_length(part_item) < 20) %>% group_by(part_item) %>% count()
 # Agrupar y contar incidencias por version de Sync
 incidenciasPorVersionSync <- datosYColumnasMejorados %>% group_by(sync_system) %>% count()
 # Agrupar y contar incidencias por concern
 incidenciasPorConcern <- datosYColumnasMejorados %>% group_by(concern) %>% count()
-# Agrupar y contar incidencias por parte y concern
-incidenciasPorParteYConcern <- datosYColumnasMejorados %>% group_by(part_item, concern) %>% count()
 
+# Graficar incidencias por millas menores a 1000
+ggplot(incidenciasPorMillas, aes(x=incidenciasPorMillas$mileage, y=incidenciasPorMillas$n)) +
+  geom_segment( aes(x=incidenciasPorMillas$mileage, xend=incidenciasPorMillas$mileage, y=0, yend=incidenciasPorMillas$n), color="green") +
+  geom_point( color="magenta", size=1, alpha=0.6) +
+  theme_light() +
+  coord_flip() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank(),
+  ) + xlab("Millas") + ylab("Número de Incidencias")
+# Graficar incidencias por version de sync
+barplot(height=incidenciasPorVersionSync$n, names=incidenciasPorVersionSync$sync_system, xlab="Versión de Sync", col = "#b400b6", main = "Incidencias por Versión de Sync")
+# Graficar incidencias por concern
+ggplot(incidenciasPorConcern, aes(x=incidenciasPorConcern$concern, y=incidenciasPorConcern$n)) +
+  geom_segment( aes(x=incidenciasPorConcern$concern, xend=incidenciasPorConcern$concern, y=0, yend=incidenciasPorConcern$n), color="skyblue") +
+  geom_point( color="blue", size=1, alpha=0.6) +
+  theme_light() +
+  coord_flip() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank(),
+  ) + xlab("Concern") + ylab("Número de Incidencias")
+
+# Graficar incidencias por parte
+ggplot(incidenciasPorParte, aes(x=incidenciasPorParte$part_item, y=incidenciasPorParte$n)) +
+  geom_segment( aes(x=incidenciasPorParte$part_item, xend=incidenciasPorParte$part_item, y=0, yend=incidenciasPorParte$n), color="cyan") +
+  geom_point( color="red", size=1, alpha=0.6) +
+  theme_light() +
+  coord_flip() +
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank(),
+  ) + xlab("Parte") + ylab("Número de Incidencias")
